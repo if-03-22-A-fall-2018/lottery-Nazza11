e41517978a8519f2bdbb21e1a7a32d357ec57a6f
Vinzent Kronreif <vini.kro@gmail.com>
2019-04-28 22:26:18 +0200

    a

--- assignment/lottery.h
+++ assignment/lottery.h
@@ -10,18 +10,20 @@
  * Header for lottery tips analysis.
  * ----------------------------------------------------------
  */
-#ifndef ___LOTTERY_H
-#define ___LOTTERY_H
+ #ifndef ___LOTTERY_H
+ #define ___LOTTERY_H
 
-#define TIP_SIZE 6
+ #include "general.h"
+ #define TIP_SIZE 6
 
-bool init_lottery(const char *csv_file, char csv_separator);
+ bool init_lottery(const char *csv_file, char csv_separator);
 
-bool get_tip(int tip_number, int tip[TIP_SIZE]);
+ bool get_tip(int tip_number, int tip[TIP_SIZE]);
 
-bool set_drawing(int drawing_numbers[TIP_SIZE]);
+ bool set_drawing(int drawing_numbers[TIP_SIZE]);
 
-int get_tip_result(int tip_number);
+ int get_tip_result(int tip_number);
 
-int get_right_tips_count(int right_digits_count);
-#endif
+ int get_right_tips_count(int right_digits_count);
+
+ #endif
